d467807397e7f8f3da935947318c0ccf1c77dde4
RJ <r.j.chomik@gmail.com>
2020-11-14 15:49:17 +0000

    Account model implemented

---
+++ FirstCEP/Model/Account.swift
@@ -0,0 +1,21 @@
+//
+//  Account.swift
+//  FirstCEP
+//
+//  Created by The App Experts on 14/11/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import Foundation
+
+struct Accounts :Decodable{
+    let accounts:[Account]
+}
+
+struct Account:Decodable {
+    let kind:String
+    let title:String
+    let number:String
+    let balance:Double
+    let currency:String
+}
