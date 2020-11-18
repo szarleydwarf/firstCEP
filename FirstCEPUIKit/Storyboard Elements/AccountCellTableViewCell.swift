4709315a98ba6d5c6fe4cc9cb4af15db8bc033c5
RJ <r.j.chomik@gmail.com>
2020-11-18 17:25:34 +0000

    custom UITableCell implementation

---
+++ FirstCEPUIKit/Storyboard Elements/AccountCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountCellTableViewCell.swift
+//  FirstCEPUIKit
+//
+//  Created by The App Experts on 18/11/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class AccountCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
