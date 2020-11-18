d056594c456b3972c78e0296e33ca66c840d2d88
RJ <r.j.chomik@gmail.com>
2020-11-18 16:29:44 +0000

    storyboard version of CEP

---
+++ FirstCEPUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstCEPUIKit
+//
+//  Created by The App Experts on 18/11/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
