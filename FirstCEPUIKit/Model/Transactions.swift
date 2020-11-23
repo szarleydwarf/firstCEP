245c3b8f5b184f39f2a608ec74b7141779ca7275
RJ <r.j.chomik@gmail.com>
2020-11-23 12:28:17 +0000

    update on the transaction file and object

--- FirstCEPUIKit/Model/Transactions.swift
+++ FirstCEPUIKit/Model/Transactions.swift
@@ -8,36 +8,23 @@
 
 import Foundation
 
-struct AllTransactions:Decodable {
-    let allTransactions:String
-    
-    enum CodingKeys: String, CodingKey {
-        case allTransactions = "all_transactions"
-    }
-}
 
 struct accountTransactions:Decodable {
-    let senderAccount:String
     let transactions: [Transactions]
-    
-    enum CodingKeys: String, CodingKey {
-        case senderAccount = "sender_account"
-        case transactions
-    }
 }
 
 struct Transactions: Decodable {
     let dateOfTransaction:String
-    let amount:String
-    let type:String
-    let currency:String
-    let recipient:String
+//    let amount:String
+//    let type:String
+//    let currency:String
+//    let recipient:String
     
     enum CodingKeys: String, CodingKey {
         case dateOfTransaction = "data_of_transaction"
-        case amount
-        case type
-        case currency
-        case recipient
+//        case amount
+//        case type
+//        case currency
+//        case recipient
     }
 }
