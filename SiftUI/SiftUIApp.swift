3e32267a0ebae2c91f529e29813923bb4c1d4fda
RJ <r.j.chomik@gmail.com>
2021-04-26 18:45:52 +0100

    starting over initial

---
+++ SiftUI/SiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiftUIApp.swift
+//  SiftUI
+//
+//  Created by The App Experts on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
