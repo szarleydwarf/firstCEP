563395931cb22d4edc28188b044c0e5c90fe4606
RJ <r.j.chomik@gmail.com>
2021-05-18 16:51:31 +0100

    basic structure

---
+++ PokiMVVMUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokiMVVMUI
+//
+//  Created by The App Experts on 18/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokiMVVMUI/PokiMVVMUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokiMVVMUIApp.swift
+//  PokiMVVMUI
+//
+//  Created by The App Experts on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokiMVVMUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
