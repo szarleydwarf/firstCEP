f54b049bc5f8cb499e7512da2cfb9d0eb03427cd
RJ <r.j.chomik@gmail.com>
2021-05-19 16:26:57 +0100

    vm created, subview for image created

---
+++ PokiMVVMUI/SubViews/MobiImageView.swift
@@ -0,0 +1,20 @@
+//
+//  MobiImageView.swift
+//  PokiMVVMUI
+//
+//  Created by The App Experts on 19/05/2021.
+//
+
+import SwiftUI
+
+struct MobiImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MobiImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MobiImageView()
+    }
+}
---
+++ PokiMVVMUI/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  PokiMVVMUI
+//
+//  Created by The App Experts on 19/05/2021.
+//
+
+import Foundation
