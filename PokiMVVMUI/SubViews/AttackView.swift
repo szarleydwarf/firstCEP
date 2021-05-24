e13566fdd61d4fa01035492fa97b2559f1dd786c
RJ <r.j.chomik@gmail.com>
2021-05-24 21:00:06 +0100

    adding scroll view, battle system, attacj=k view

---
+++ PokiMVVMUI/SubViews/AttackView.swift
@@ -0,0 +1,20 @@
+//
+//  AttackView.swift
+//  PokiMVVMUI
+//
+//  Created by The App Experts on 24/05/2021.
+//
+
+import SwiftUI
+
+struct AttackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AttackView_Previews: PreviewProvider {
+    static var previews: some View {
+        AttackView()
+    }
+}
