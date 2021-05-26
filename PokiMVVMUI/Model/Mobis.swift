488932f5407841b29b544239bd523ed25603c008
RJ <r.j.chomik@gmail.com>
2021-05-26 13:14:38 +0100

    changing the json file and model design

---
+++ PokiMVVMUI/Model/Mobis.swift
@@ -0,0 +1,25 @@
+//
+//  Mobis.swift
+//  PokiMVVMUI
+//
+//  Created by The App Experts on 18/05/2021.
+//
+
+import Foundation
+
+struct MobiElements: Decodable, Hashable, Identifiable {
+    var id: UUID
+    var name: String
+    var items: [Mobi]
+
+}
+
+struct Mobi: Decodable, Hashable, Identifiable {
+    let id: UUID
+    let name, element: String
+    let attack, defence, exp, life: Int
+
+    #if DEBUG
+    static var example = Mobi(id: UUID(), name: "", element: "", attack: 0, defence: 0, exp: 0, life: 0)
+    #endif
+}
